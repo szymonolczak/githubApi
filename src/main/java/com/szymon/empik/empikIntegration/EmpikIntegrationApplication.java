5dc7d1c7a17a6bdb91a33752ef18a34599b78fc0
szymon.olczak <szymon.olczak@gft.com>
2021-06-11 00:08:54 +0200

    init project

---
+++ src/main/java/com/szymon/empik/empikIntegration/EmpikIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.szymon.empik.empikIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpikIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpikIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymon/empik/empikIntegration/EmpikIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymon.empik.empikIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
